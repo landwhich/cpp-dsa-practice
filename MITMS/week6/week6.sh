3e090eaa3721526f211139945f4f21aaa4b351c5
Landwhich <156349813+Landwhich@users.noreply.github.com>
2026-05-20 15:24:10 -0400

    Done week 6

---
+++ MITMS/week6/week6.sh
@@ -0,0 +1,9 @@
+### packaging and shipping code
+
+# 1
+# made pyenv.txt in python venv and env.txt my env
+# path and venv varibles are different
+printenv # was used to print the environment
+
+# 3
+
