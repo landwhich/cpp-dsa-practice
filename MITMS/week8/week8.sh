b0fd084aa38d3a1bdf47c4897a48f02350d7111c
Landwhich <156349813+Landwhich@users.noreply.github.com>
2026-05-23 17:12:50 -0400

    week 8 done

---
+++ MITMS/week8/week8.sh
@@ -0,0 +1,20 @@
+# Security and Privacy
+
+# 1 Entropy
+
+# 1.1
+# 66 bits = log2 ((100_000)^4)
+
+# 1.2
+# 47 bits = log2 ((26 x 2 + 10)^8)
+
+# 1.4 
+# 1.2 would take 1.5 days to crack at a rate of 
+# 1.4 would take 1.2 billion years
+# that, a standard alphanum pass from a password manager (say 22 chars) would 2.7 × 10³⁴ years, vastly exceeding the age of the universe
+
+# 2 Hashing (sha256sum)
+# checking the hashes of mirror and official should result in the same
+# if both are uncorrupted and non-fraudulent
+
+# 
